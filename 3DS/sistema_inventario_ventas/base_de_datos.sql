cba8606d9f184c8722e5832f992029f39ca5d9ed
ogaldamez738-wq <ogaldamez738@gmail.com>
2026-05-14 21:04:17 -0600

    Agregado diseño inicial de la base de datos

---
+++ 3DS/sistema_inventario_ventas/base_de_datos.sql
@@ -0,0 +1,17 @@
+USE sistema_inventario;
+
+CREATE TABLE usuarios (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre_completo VARCHAR(100) NOT NULL,
+ usuario VARCHAR(50) NOT NULL UNIQUE,
+ password VARCHAR(255) NOT NULL,
+ rol VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE productos (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ nombre_producto VARCHAR(100) NOT NULL,
+ categoria VARCHAR(50) NOT NULL,
+ stock INT NOT NULL,
+ precio DECIMAL(10, 2) NOT NULL
+);
